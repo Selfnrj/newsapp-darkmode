6375b7227430c34b3892cd836334f6185a4af6c7
Ambjörn Fagerström <ambjorn89@gmail.com>
2022-12-08 00:10:26 +0100

    Add news API

---
+++ app/page.tsx
@@ -0,0 +1,23 @@
+﻿import { categories } from "../constants"
+import fetchNews from "../lib/fetchNews"
+
+async function Homepage() {
+
+  const news: NewsResponse = await fetchNews(categories.join(","));
+
+  console.log(news);
+
+  return (
+    <div>Homepage
+      {news.data.map((item) => (
+        <div className="dark:text-white" key={item.title}>
+          <h1>{item.title}</h1> 
+          <p>{item.author}</p>
+          <p>{item.description}</p>
+        </div>
+      ))}
+    </div> 
+  )
+}
+
+export default Homepage
---
+++ constants.ts
@@ -0,0 +1,9 @@
+﻿export const categories: Category[] = [
+ "general",
+ "business",
+ "entertainment",
+ "health",
+ "science",
+ "sports",
+ "technology",
+];
